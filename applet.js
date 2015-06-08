b6bc80775563822e67e613a177562f3ee2347584
BlueC0re <coding@bluec0re.eu>
2015-06-08 11:36:51 +0200

    added port list

--- applet.js
+++ applet.js
@@ -1,11 +1,18 @@
 const Lang = imports.lang;
 const Applet = imports.ui.applet;
+const PopupMenu = imports.ui.popupMenu;
 const GLib = imports.gi.GLib;
 const Gio = imports.gi.Gio;
+const St = imports.gi.St;
 const Gettext = imports.gettext.domain('cinnamon-applets');
 const _ = Gettext.gettext;
+const AppletDir = imports.ui.appletManager.applets['daemon-info@bluec0re.eu'];
 
 const Mainloop = imports.mainloop;
+
+const TerminalReader = AppletDir.lib.TerminalReader;
+const bind = AppletDir.lib.bind;
+const PortList = AppletDir.portlist.PortList;
 
 
 function DaemonInfo(metadata, orientation) {
@@ -18,10 +25,20 @@ DaemonInfo.prototype = {
 
     _init: function(orientation) {
         Applet.TextIconApplet.prototype._init.call(this, orientation);
+        this._orientation = orientation;
 
         try {
             this.set_applet_icon_name(this.metadata.icon);
             this._updateLabel();
+
+            this.menuManager = new PopupMenu.PopupMenuManager(this);
+            this._initMenu();
+
+            this._portList = new PortList();
+            this._portListArea = new St.BoxLayout({name: 'portListArea'});
+            this._portListArea.add(this._portList.actor);
+            this.menu.addActor(this._portListArea);
+            this._portListArea.show_all();
         } catch(e) {
             global.logError(e);
         }
@@ -55,175 +72,40 @@ DaemonInfo.prototype = {
             self.set_applet_label("" + totalPorts + ' (' + globalPorts + 'g,' + localPorts + 'l,' + tcpPorts + 't,' + udpPorts + 'u)');
         });
         tr.executeReader();
-        Mainloop.timeout_add(2000, bind(this._updateLabel, this));
+        this._timeoutId = Mainloop.timeout_add(2000, bind(this._updateLabel, this));
+    },
+
+    on_applet_clicked: function(event) {
+        this.menu.toggle();
+    },
+
+    on_applet_remove_from_panel: function() {
+        if(this._timeoutId) {
+            Mainloop.source_remove(this._timeoutId);
+        }
+    },
+
+    _initMenu: function() {
+        if(this._portListArea) this._portListArea.unparent();
+        if(this.menu) this.menuManager.removeMenu(this.menu);
+
+        this.menu = new Applet.AppletPopupMenu(this, this._orientation);
+        this.menuManager.addMenu(this.menu);
+
+        if(this._portListArea) {
+            this.menu.addActor(this._portListArea);
+            this._portListArea.show_all();
+        }
+
+        this.menu.connect('open-state-changed', Lang.bind(this, function(menu, isOpen) {
+            if(isOpen) {
+                this._portList.update();
+            }
+        }));
     }
 }
 
 function main(metadata, orientation) {
     let applet = new DaemonInfo(metadata, orientation);
     return applet;
-}
-
-function TerminalReader(command, callback) {
-   this._init(command, callback);
-}
-
-TerminalReader.prototype = {
-   _init: function(command, callback) {
-      this._callbackPipe = callback;
-      this._commandPipe = command;
-      this.idle = true;
-      this._childWatch = null;
-   },
-
-   executeReader: function() {
-      if(this.idle) {
-         this.idle = false;
-         try {
-            let [success, argv] = GLib.shell_parse_argv("/usr/bin/sh -c '" + this._commandPipe + "'");
-            if(success) {
-               let [exit, pid, stdin, stdout, stderr] =
-                    GLib.spawn_async_with_pipes(null,
-                                                argv, 
-                                                null,
-                                                GLib.SpawnFlags.SEARCH_PATH | GLib.SpawnFlags.DO_NOT_REAP_CHILD, 
-                                                null );
-
-               this._childPid = pid;
-               this._stdin = new Gio.UnixOutputStream({ fd: stdin, close_fd: true });
-               this._stdout = new Gio.UnixInputStream({ fd: stdout, close_fd: true });
-               this._stderr = new Gio.UnixInputStream({ fd: stderr, close_fd: true });
-
-               // We need this one too, even if don't actually care of what the process
-               // has to say on stderr, because otherwise the fd opened by g_spawn_async_with_pipes
-               // is kept open indefinitely
-               this._stderrStream = new Gio.DataInputStream({ base_stream: this._stderr });
-               this._dataStdout = new Gio.DataInputStream({ base_stream: this._stdout });
-               this._cancellableStderrStream = new Gio.Cancellable();
-               this._cancellableStdout = new Gio.Cancellable();
-
-               this.resOut = 1;
-               this._readStdout();
-               this.resErr = 1;
-               this._readStderror();
-
-               this._childWatch = GLib.child_watch_add(GLib.PRIORITY_DEFAULT, pid, Lang.bind(this, function(pid, status, requestObj) {
-                  GLib.source_remove(this._childWatch);
-                  this._childWatch = null;
-                  this._stdin.close(null);
-                  this.idle = true;
-               }));
-            }
-            //throw
-         } catch(err) {
-            if (err.code == GLib.SpawnError.G_SPAWN_ERROR_NOENT) {
-//               err.message = _("Command not found.");
-            } else {
-               // The exception from gjs contains an error string like:
-               //   Error invoking GLib.spawn_command_line_async: Failed to
-               //   execute child process "foo" (No such file or directory)
-               // We are only interested in the part in the parentheses. (And
-               // we can't pattern match the text, since it gets localized.)
-               err.message = err.message.replace(/.*\((.+)\)/, '$1');
-            }
-            throw err;
-         }
-      }
-   },
-
-   destroy: function() {
-      try {
-         if(this._childWatch) {
-            GLib.source_remove(this._childWatch);
-            this._childWatch = null;
-         }
-         if(!this._dataStdout.is_closed()) {
-            this._cancellableStdout.cancel();
-            this._stdout.close_async(0, null, Lang.bind(this, this.closeStdout));
-         }
-         if(!this._stderrStream.is_closed()) {
-            this._cancellableStderrStream.cancel();
-            this._stderrStream.close_async(0, null, Lang.bind(this, this.closeStderrStream));
-         }
-         this._stdin.close(null);
-         this.idle = true;
-      }
-      catch(e) {
-         Main.notify("Error on close" + this._dataStdout.is_closed(), e.message);
-      }
-   },
-
-   closeStderrStream: function(std, result) {
-      try {
-        std.close_finish(result);
-      } catch(e) {
-         std.close_async(0, null, Lang.bind(this, this.closeStderrStream));
-      }
-   },
-
-   closeStdout: function(std, result) {
-      try {
-        std.close_finish(result);
-      } catch(e) {
-         std.close_async(0, null, Lang.bind(this, this.closeStderrStream));
-      }
-   },
-
-   _readStdout: function() {
-      this._dataStdout.fill_async(-1, GLib.PRIORITY_DEFAULT, this._cancellableStdout, Lang.bind(this, function(stream, result) {
-         try {
-            if(!this._dataStdout.is_closed()) {
-               if(this.resOut != -1)
-                  this.resOut = this._dataStdout.fill_finish(result);// end of file
-               if(this.resOut == 0) {
-                  let val = stream.peek_buffer().toString();
-                  if(val != "")
-                     this._callbackPipe(this._commandPipe, true, val);
-                  this._stdout.close(this._cancellableStdout);
-               } else {
-                  // Try to read more
-                  this._dataStdout.set_buffer_size(2 * this._dataStdout.get_buffer_size());
-                  this._readStdout();
-               }
-            }
-         } catch(e) {
-            global.log(e);
-         }
-      }));
-   },
-
-   _readStderror: function() {
-      this._stderrStream.fill_async(-1, GLib.PRIORITY_DEFAULT, this._cancellableStderrStream, Lang.bind(this, function(stream, result) {
-         try {
-            if(!this._stderrStream.is_closed()) {
-               if(this.resErr != -1)
-                  this.resErr = this._stderrStream.fill_finish(result);
-               if(this.resErr == 0) { // end of file
-                  let val = stream.peek_buffer().toString();
-                  if(val != "")
-                     this._callbackPipe(this._commandPipe, false, val);
-                  this._stderr.close(null);
-               } else {
-                  this._stderrStream.set_buffer_size(2 * this._stderrStream.get_buffer_size());
-                  this._readStderror();
-               }
-            }
-         } catch(e) {
-            global.log(e);
-         }
-      }));
-   }
-};
-
-function bind(func, context){
-    function callback(){
-        try {
-            return func.apply(context, arguments);
-        } catch(e){
-            global.logError(e);
-            return null;
-        }
-    }
-
-    return callback;
 }
---
+++ portlist.js
@@ -0,0 +1,56 @@
+const St = imports.gi.St;
+const AppletDir = imports.ui.appletManager.applets['daemon-info@bluec0re.eu'];
+const TerminalReader = AppletDir.lib.TerminalReader;
+
+function PortList() {
+    this._init();
+}
+
+PortList.prototype = {
+    _init: function() {
+        this.actor = new St.Table({
+            homogeneous: false
+        });
+        this.actor.set_style('padding: 20px');
+    },
+
+    update: function() {
+        this.actor.destroy_children();
+        let actor = this.actor;
+
+        actor.add(new St.Label({text: 'Protocol', style: 'padding-right: 5px'}), {row: 0, col: 0});
+        actor.add(new St.Label({text: 'Address', style: 'padding-right: 5px; padding-left: 5px'}), {row: 0, col: 1});
+        actor.add(new St.Label({text: 'Port', style: 'padding-left: 5px'}), {row: 0, col: 2});
+
+        let tr = new TerminalReader('/usr/bin/ss -ltun', function(cmd, success, result) {
+            let lines = result.split('\n');
+            for(let i = 1; i < lines.length; i++) {
+                let line = lines[i];
+                line = line.replace(/ +/g, ' ').split(' ');
+                let proto = line[0];
+                let state = line[1];
+                let recv = line[2];
+                let send = line[3];
+                let laddr = line[4].split(':');
+                if(laddr.length > 2) {
+                    let port = laddr.pop();
+                    laddr = [laddr.join(':'), port];
+                }
+                let raddr = line[5].split(':');
+                if(raddr.length > 2) {
+                    let port = raddr.pop();
+                    raddr = [raddr.join(':'), port];
+                }
+
+                actor.add(new St.Label({text: proto, style: 'padding-right: 5px'}), {row: i, col: 0});
+                actor.add(new St.Label({text: laddr[0], style: 'padding-right: 5px; padding-left: 5px'}), {row: i, col: 1});
+                actor.add(new St.Label({text: laddr[1], style: 'padding-left: 5px'}), {row: i, col: 2});
+            }
+
+            for(let child of actor.get_children()) {
+                child.get_children()[0].set_style('padding-left: 1em; padding-right: 1em;');
+            }
+        });
+        tr.executeReader();
+    }
+};
